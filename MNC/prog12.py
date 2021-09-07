1ec3703a1b2609b64c864b5f84e892696b162fe2
Anudeep Gunukula <55506841+AnudeepGunukula@users.noreply.github.com>
2021-09-07 21:00:02 +0530

    Update prog12.py

--- MNC/prog12.py
+++ MNC/prog12.py
@@ -1,26 +1,27 @@
-m,n=input().split()
+l=[int(i) for i in input().split()]
+a,b=l[0],l[1]
+
 biglist=[]
-for i in range(int(n)):
-    a,b=input().split()
-    if i == 0:
-        biglist.append([a, b])
-        continue
+for i in range(b):
     flag=0
-    for x in biglist:
-        if a in x:
-            x.append(b)
-            flag=1
-    if flag==0:
-        biglist.append([a, b])
-ans=100000000
+    z=[int(i) for i in input().split()]
+    x,y=z[0],z[1]
+    if i==0:
+        biglist.append([x,y])
+    else:
+        for i in biglist:
+            if x in i:
+                i.append(y)
+                flag=1
+                continue
+            if y in i:
+                i.append(x)
+                flag=1
+        if flag==0:
+            biglist.append([x,y])
+min=len(biglist[0])
 for i in biglist:
-    if len(i)<ans:
-        ans=len(i)
-print(ans)
-
-
-
-
-
-
-
+    if len(i) < min:
+        min=len(i)
+        
+print(min)
